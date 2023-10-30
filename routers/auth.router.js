35ad7d32510fafff6fb029eac360e5a2ff80d2e8
JoshMusira <joshuamusira01@gmail.com>
2023-10-31 00:01:38 +0300

    feat: register a new user

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,7 +1,28 @@
 import { Router } from "express";
+import User from '../models/user.model.js'
+import bcryt from 'bcrypt'
 
 export const authRouter = Router();
 
-authRouter.get("/auth", async (req, res) => {
+//REGISTER USER
+authRouter.post("/auth/register", async (req, res) => {
+
+    try {
+        const salt = await bcryt.genSalt(10);
+        const hashedPassword = await bcryt.hash(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+
+        const user = await newUser.save();
+        res.status(200).json(user);
+
+    } catch (error) {
+        console.log(error)
+    }
 
 });
