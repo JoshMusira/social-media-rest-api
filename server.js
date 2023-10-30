7c7de0475034f4512864a3262475004f51913bf9
JoshMusira <joshuamusira01@gmail.com>
2023-10-30 19:13:47 +0300

    feat: configure project

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("Server running")
+import express from 'express'
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+const port = process.env.PORT || 5000
+
+
+
+const app = express();
+
+app.use(
+    cors({
+        origin: '*',
+        optionsSuccessStatus: 200,
+    })
+)
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Social media rest api !!!!");
+});
+
+
+app.listen(port, () =>
+    console.log(`Backend running on port ${port}`)
+)
