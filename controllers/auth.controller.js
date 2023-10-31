7fbf74aee0b1a5db622e12041a6589542f0c52b1
JoshMusira <joshuamusira01@gmail.com>
2023-10-31 10:41:17 +0300

    feat: login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // REGISTER USER
 export const RegisterUser = async (req, res) => {
@@ -35,18 +36,43 @@ export const RegisterUser = async (req, res) => {
             });
         }
     } catch (error) {
-        console.log(error);
+        res.status(500).json("Internal Server Error");
     }
 };
 
 
-//LOGIN USER
+// LOGIN USER
 export const LoginUser = async (req, res) => {
+    const { password, email } = req.body;
     try {
-        const user = await User.findOne({ email: req.body.email });
-        !user && res.status(404).json("User not found");
+        const user = await User.findOne({ email });
 
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({ message: "Wrong Credentials" });
+        }
+
+        const payload = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+        });
+
+        res.status(200).json({
+            message: "Logged in successfully",
+            token,
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
